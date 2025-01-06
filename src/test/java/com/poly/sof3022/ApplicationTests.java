ac17ec01f77d61f3906c786b03c2f0379ab39a09
HangNT169 <HangNT169@fpt.edu.vn>
2025-01-06 18:59:23 +0700

    Init project

---
+++ src/test/java/com/poly/sof3022/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.sof3022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
